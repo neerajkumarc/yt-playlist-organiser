ae102f55a2646ed2d92a12db12df657e615c6336
neerajkumarc <chaudharyneeraj91@gmail.com>
2025-01-11 19:55:38 +0530

    ui: responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -71,10 +71,10 @@ export default async function Home() {
       <div className="container mx-auto px-4 sm:px-6 py-8">
         <div className="backdrop-blur-lg rounded-3xl shadow-xl border border-white/10 p-6 md:p-8">
           <div className="flex flex-col sm:flex-row items-start sm:items-center justify-between gap-4 mb-8">
-            <h1 className="text-3xl md:text-4xl font-bold ">
+            <h1 className="text-2xl md:text-4xl font-bold ">
               Youtube Playlist Organizer
             </h1>
-            <div className="flex items-start sm:items-center gap-4">
+            <div className="flex flex-col md:flex-row items-start sm:items-center gap-4">
               <div className="flex items-center gap-2 px-4 py-2 rounded-full bg-white/5 backdrop-blur-sm border border-white/10">
                 <UserCircle className="w-5 h-5" />
                 <span className="text-sm truncate max-w-[200px]">
